55a27ec6b6a9042af594299336eae8a00eba12e8
Lucas Costa <lucas.gustavoc@live.com>
2022-05-17 17:12:51 -0300

    server on

---
+++ index.js
@@ -0,0 +1,10 @@
+const router = require("./router")
+const express = require("express")
+const app = express()
+const porta = 3000
+
+app.get('/', (req, res) => res.send('API On'));
+app.use(express.json())
+app.use(router)
+
+app.listen(porta, () => console.log(`Servidor ativo na porta ${porta}`))
---
+++ router.js
@@ -0,0 +1,12 @@
+const router = new require("express").Router()
+const axios = require("axios")
+
+const urlProfissionais = 'https://api-homolog.geracaopet.com.br/api/challenges/challenge1/employees'
+
+router.get('/agenda', async (req, res) => {
+    let obj = await axios.get(urlProfissionais)
+    console.log(obj.data)
+    res.json(obj.data)
+})
+
+module.exports = router;
