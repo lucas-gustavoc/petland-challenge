013b204f1e92db45177ab721b32dff96654b2bf2
Lucas Costa <lucas.gustavoc@live.com>
2022-05-17 23:35:13 -0300

    provisório finalizado

--- router.js
+++ router.js
@@ -1,13 +1,10 @@
 const router = new require("express").Router()
-const AgendaDoDia = require("./AgendaDoDia")
+const encontrarHorariosDisponiveis = require("./ModeloDeAgenda")
 const axios = require("axios")
 
-const urlProfissionais = 'https://api-homolog.geracaopet.com.br/api/challenges/challenge1/employees'
-
 router.get('/agenda', async (req, res) => {
-    let obj = await axios.get(urlProfissionais)
-    console.log(obj.data)
-    res.json(obj.data)
+    let availableTimes = await encontrarHorariosDisponiveis()
+    res.json(availableTimes)
 })
 
 module.exports = router;
